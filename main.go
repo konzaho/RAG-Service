2d6a5f3b290cfdc8121a9839bf1c9c633bb41cf9
Fiqih Rhamdani <fiqih.rhamdani@gmail.com>
2020-03-05 17:24:53 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type Request struct {
+	Nickname  string
+	Locations []string
+	Times     []string
+	Factions  []string
+}
+
+type Response struct {
+	Location string
+	Time     string
+	Faction  string
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/rag", homeLink).Methods("POST")
+	router.HandleFunc("/rag/generate", generateLevel).Methods("POST").Headers("Content-Type", "application/json")
+
+	log.Print(http.ListenAndServe(":8080", router))
+}
+
+func generateLevel(w http.ResponseWriter, r *http.Request) {
+	var request Request
+	var response Response
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprint(w, "Error on parsing body request")
+	} else {
+		json.Unmarshal(reqBody, &request)
+		response.Location = request.Locations[randomIndex(len(request.Locations))]
+		response.Time = request.Times[randomIndex(len(request.Times))]
+		response.Faction = request.Factions[randomIndex(len(request.Factions))]
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func randomIndex(max int) int {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	return rand.Intn(max-min) + min
+}
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Randomize your ArmA Escape scenario with this service! -by: KonZaho")
+}
